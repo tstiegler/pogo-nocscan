0c39cc95cd75887b1de346950218dbb0e2dc2ff7
tstiegler <stiegler.t@gmail.com>
2016-11-17 21:57:19 -0500

    Refactoring + auto config loading.

--- pogo-nocscan/configloader.js
+++ pogo-nocscan/configloader.js
@@ -1,21 +1,25 @@
 var _       = require("lodash");
 var fs      = require("fs");
 
-// Get a file list.
-var files = fs.readdirSync('./', {encoding: 'utf-8'});
-var config = [];
+function loadConfig() {
+    // Get a file list.
+    var files = fs.readdirSync('./', {encoding: 'utf-8'});
+    var config = [];
 
-// Iterate over all files.
-_.each(files, function(file) {
-    // If this is a config file, load it and push the result into the config array.
-    if(file.startsWith("config.") && file.endsWith(".js")) {
-        var loadedConf = require(file);
+    // Iterate over all files.
+    _.each(files, function(file) {
+        // If this is a config file, load it and push the result into the config array.
+        if(file.startsWith("config.") && file.endsWith(".js")) {
+            var loadedConf = require("./" + file);
 
-        if(loadedConf instanceof Array) 
-            _.each(loadedConf, function(item) { config.push(item); });
-        else
-            config.push(loadedConf);
-    }
-})
+            if(loadedConf instanceof Array) 
+                _.each(loadedConf, function(item) { config.push(item); });
+            else
+                config.push(loadedConf);
+        }
+    });
 
-module.exports = config;
+    return config;
+}
+
+module.exports = loadConfig;
